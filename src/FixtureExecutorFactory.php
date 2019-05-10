8cabe180c681a118cecff023c60181ca44dd1428
Adrien LUCAS <adrien.lucas@smile.fr>
2019-05-10 21:17:19 +0200

    Add FixtureExecutorFactory

---
+++ src/FixtureExecutorFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adrien;
+
+use Doctrine\Common\DataFixtures\Executor\AbstractExecutor;
+use Doctrine\Common\DataFixtures\Executor\MongoDBExecutor;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Executor\PHPCRExecutor;
+use Doctrine\Common\DataFixtures\Purger\MongoDBPurger;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\Common\DataFixtures\Purger\PHPCRPurger;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ODM\PHPCR\DocumentManagerInterface;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use LogicException;
+
+class FixtureExecutorFactory
+{
+    public static function createManagerExecutor(ObjectManager $manager): AbstractExecutor
+    {
+        if($manager instanceof EntityManagerInterface) {
+            return new ORMExecutor($manager, new ORMPurger($manager));
+        }
+
+        if($manager instanceof DocumentManagerInterface) {
+            return new PHPCRExecutor($manager, new PHPCRPurger($manager));
+        }
+
+        if($manager instanceof DocumentManager) {
+            return new MongoDBExecutor($manager, new MongoDBPurger($manager));
+        }
+
+        throw new LogicException(sprintf('No fixture executor found for "%s".', get_class($manager)));
+    }
+}
---
+++ tests/FixtureExecutorFactoryTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adrien\Tests;
+
+
+use Adrien\FixtureExecutorFactory;
+use Doctrine\Common\DataFixtures\Executor\MongoDBExecutor;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Executor\PHPCRExecutor;
+use Doctrine\Common\DataFixtures\Purger\MongoDBPurger;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\Common\DataFixtures\Purger\PHPCRPurger;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ODM\MongoDB\DocumentManager as MongoDBDocumentManager;
+use Doctrine\ODM\PHPCR\DocumentManager as PHPCRDocumentManager;
+use Doctrine\ORM\EntityManagerInterface;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class FixtureExecutorFactoryTest extends TestCase
+{
+    /**
+     * @dataProvider provideManagersWithExpectedExecutor
+     */
+    public function testItReturnsTheRightExecutorForTheGivenManager($givenManager, $expectedExecutor, $expectedPurger)
+    {
+        $executor = FixtureExecutorFactory::createManagerExecutor($givenManager);
+
+        static::assertInstanceOf(
+            $expectedExecutor,
+            $executor
+        );
+
+        static::assertInstanceOf(
+            $expectedPurger,
+            $executor->getPurger()
+        );
+    }
+
+    public function provideManagersWithExpectedExecutor()
+    {
+        yield 'orm' => [
+            static::createConfiguredMock(EntityManagerInterface::class, ['getEventManager' => new EventManager()]),
+            ORMExecutor::class,
+            ORMPurger::class
+        ];
+
+        yield 'phpcr' => [
+            static::createMock(PHPCRDocumentManager::class),
+            PHPCRExecutor::class,
+            PHPCRPurger::class
+        ];
+
+        yield 'mongodb' => [
+            static::createConfiguredMock(MongoDBDocumentManager::class, ['getEventManager' => new EventManager()]),
+            MongoDBExecutor::class,
+            MongoDBPurger::class
+        ];
+    }
+
+    public function testItThrowsAnExceptionWhenNoExecutorFound()
+    {
+        static::expectException(LogicException::class);
+        FixtureExecutorFactory::createManagerExecutor(static::createMock(ObjectManager::class));
+    }
+}
+
